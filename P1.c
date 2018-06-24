d9cea5a02ed52686d412f66e673e1450ec358ea7
Darkfirexp <38019425+Darkfirexp@users.noreply.github.com>
2018-06-23 23:26:41 -0300

    Add files via upload

--- P1.c
+++ P1.c
@@ -1,44 +1,53 @@
 #include <stdio.h>
-
-int main(){
-	int est = 0, i = 0;
-    char bits[256];
-    printf("Digite um numero que seja binario: \n");
-    scanf("%s", bits);
-    while(bits[i] != '\0'){
-        if (est == 0 && bits[i] == '0'){
-    est = 0;
-    i++;
-	}
-    if (est == 0 && bits[i] == '1'){
-    est = 1;
-    i++;
-    }
-    if (est == 1 && bits[i] == '0'){
-    est = 2;
-    i++;
-    }
-    if (est == 1 && bits[i] == '1'){
-    est = 0;
-    i++;
-    }
-    if (est == 2 && bits[i] == '0'){
-    est = 1;
-    i++;
-    }
-    if (est == 2 && bits[i] == '1'){
-    est = 2;
-    i++;
-    }
-	}
-	if (est == 0){
-	printf("\nO numero binario eh: %s", &bits);
-	printf("\nO numero eh multiplo de 3.");
-	}
-	else{
-	printf("\nO numero binario eh: %s", &bits);
-	printf("\nO numero nao eh multiplo de 3.");
-	}
-	return 0;
 	
+int main ()
+{
+	int est = 0, n = 0;
+	char bits [256];
+	printf ("\nInsira um numero em binario: ");
+	scanf ("%s", bits);
+			
+	while(bits[n]!='\0')
+	{
+		if(bits[n]=='0')
+		{
+			if(est==0)
+				est=0;
+			else
+			{
+				if(est==1)
+					est=2;
+				else
+				{
+					if (est==2)
+						est=1;
+				}
+			}	
+		}	
+		if(bits[n]=='1')
+		{
+			if(est==0)
+				est=1;
+			else
+			{
+				if(est==1)
+					est=0;
+				else
+				{
+					if(est=2)
+						est=2;
+				}
+				
+			}
+		}
+		n++;
+	}
+	
+	if (est==0)
+		printf("\nO numero e multiplo de 3...",bits);
+	if (est!=0)
+		printf("\nO numeronao eh multiplo de 3...", bits);
+
+	return 0;
+
 }
